e8c1709284315206560f5bd7a006880fada1a841
pc <pc@172.16.14.118>
2016-05-27 10:08:37 -0600

    clase

---
+++ src/ejercicio1/ClasePerona.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ejercicio1;
+
+
+public class ClasePerona {
+    private String nombre;
+    private String marca;
+    private double precio;
+    private int existencia;
+
+    public ClasePerona() {
+    }
+
+    public ClasePerona(String nombre, String marca, double precio, int existencia) {
+        this.nombre = nombre;
+        this.marca = marca;
+        this.precio = precio;
+        this.existencia = existencia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getExistencia() {
+        return existencia;
+    }
+
+    public void setExistencia(int existencia) {
+        this.existencia = existencia;
+    }
+    
+    
+    
+}
