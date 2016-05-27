7fabd3d873e91283166e47385d40abeb2c8a9a82
edmundo <edmundo@SuperBacteria>
2016-05-27 09:19:01 -0600

    mi aporte es el hola mundo

--- src/ejercicio1/Ejercicio1.java
+++ src/ejercicio1/Ejercicio1.java
@@ -17,6 +17,7 @@ public class Ejercicio1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola Franklin");
     }
     
 }
